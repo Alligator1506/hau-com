75cb65a8be460a4badb8dace7822a24e5e802e4b
ALLIGATOR\quoc1 <quoc150620@gmail.com>
2024-06-23 12:29:53 +0700

    add image

--- obj/Debug/net8.0/webtmdt.AssemblyInfo.cs
+++ obj/Debug/net8.0/webtmdt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webtmdt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a037a2c0bba8e26de9d98e76989edf34d5409158")]
 [assembly: System.Reflection.AssemblyProductAttribute("webtmdt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webtmdt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
